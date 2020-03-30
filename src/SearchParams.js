0e58d732d2d41e5e8703c5136f6e2c7f6a7187ec
extrahappyperson <ht.uzpharma@gmail.com>
2020-03-30 19:40:55 +0200

    pets results included

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from "./Results";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
@@ -26,6 +27,7 @@ const SearchParams = () => {
       breed,
       type: animal,
     });
+    console.log("animals after submit", animals);
     setPets(animals || []);
   };
 
@@ -52,6 +54,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };

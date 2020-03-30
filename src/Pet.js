0721dee8d311339b35354fc99f1e05fc37de4d4b
extrahappyperson <ht.uzpharma@gmail.com>
2020-03-30 16:29:47 +0200

    eslint config

---
+++ src/Pet.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pet = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.animal}</h1>
+      <h1>{props.breed}</h1>
+    </div>
+  );
+};
+
+export default Pet;

83a65bab75e72ad60f70466409ff21e6d6654e20
extrahappyperson <ht.uzpharma@gmail.com>
2020-04-01 17:05:52 +0200

    cards done

--- src/Pet.js
+++ src/Pet.js
@@ -11,13 +11,28 @@ const Pet = (props) => {
 
   return (
     <Link to={`/details/${id}`} className="pet">
-      <div className="flex flex-row border">
-        <div className="p-2">
-          <img src={hero} className="w-32 h-32 object-cover" alt={name} />
+      <div className="flex flex-row border p-2 mt-2">
+        <div className="p-2 py-12">
+          <img
+            src={hero}
+            className="w-16 h-16 rounded-full shadow-md"
+            alt={name}
+          />
         </div>
-        <div className="p-2">
-          <h1>{name}</h1>
-          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+        <div className="p-2 mt-2">
+          <h1 className="font-bold text-xl mb-2">{name}</h1>
+          <h2 className="text-gray-700 text-base">{`${animal} — ${breed} — ${location}`}</h2>
+          <div class="mt-2">
+            <span class="inline-block bg-gray-200 rounded-full px-1 py-1 text-base font-semibold text-gray-700 mr-1 ">
+              #photography
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-1 py-1 text-base font-semibold text-gray-700 mr-1">
+              #travel
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-1 py-1 text-base font-semibold text-gray-700">
+              #winter
+            </span>
+          </div>
         </div>
       </div>
     </Link>
